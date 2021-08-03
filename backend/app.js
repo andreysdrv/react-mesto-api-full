6ef7a622c6b5e903521fd25e9572275b4227528a
Andrew <jeck666@mail.ru>
2021-08-03 19:33:24 +0300

    feat: cors and port

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,8 @@ const app = express();
 
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
+  'https://api.mesto.sidorov.nomoredomains.monster/',
+  'https://api.mesto.sidorov.nomoredomains.monster/',
   'localhost:3000',
 ];
 
