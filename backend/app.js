72aed43710d063ad52c3821c1644921388a30b27
Andrew <jeck666@mail.ru>
2021-08-04 18:47:31 +0300

    fix: fix cors err on login

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,8 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    // вернет ответ 200 с пустым телом после preflight-запроса
+    return res.status(200).send();
   }
 
   next();
