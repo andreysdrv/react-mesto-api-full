481fe72edb41a21e713343f16a664594afb9b4b3
Andrew <jeck666@mail.ru>
2021-08-04 19:22:10 +0300

    fix: fix cors err on login_5

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,8 @@ const app = express();
 
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https://mesto.frontend.sidorov.nomoredomains.monster/',
-  'https://api.mesto.sidorov.nomoredomains.monster/',
+  'https://mesto.frontend.sidorov.nomoredomains.monster',
+  'https://api.mesto.sidorov.nomoredomains.monster',
   'localhost:3000',
 ];
 
