c5d25076343d663f354f6b8e99f948657a9250b7
Andrew <jeck666@mail.ru>
2021-08-04 19:11:35 +0300

    fix: fix cors err on login_3

--- backend/app.js
+++ backend/app.js
@@ -24,26 +24,18 @@ const allowedCors = [
 ];
 
 app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const { origin } = req.headers;
+  const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // сохраняем список заголовков исходного запроса
   const requestHeaders = req.headers['access-control-request-headers'];
-  // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы из любого источника
     res.header('Access-Control-Allow-Origin', origin);
   }
-  // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    // вернет ответ 200 с пустым телом после preflight-запроса
-    res.status(200).send();
-    return;
+
+    return res.status(200).send();
   }
 
   next();
